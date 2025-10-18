f3efcc9eb9b9200060bf3d7d7dd692561f4c0907
Emessjay <jackprattporter@gmail.com>
2025-10-18 17:18:19 -0500

    fixed critical issue

--- software/TBM Controller/src/motor_test.cpp
+++ software/TBM Controller/src/motor_test.cpp
@@ -14,26 +14,38 @@
 // PURPLE: IO2 (25) -> PUL+
 // ======================================================================
 
-void pulse() {
-    // 🌀 Generate pulses to rotate motor
-    digitalWrite(PUL_PIN, HIGH);
-    delayMicroseconds(PULSE_HIGH_US);  // Adjust for speed
-    digitalWrite(PUL_PIN, LOW);
-    delayMicroseconds(PULSE_LOW_US);
-}
 // Pin assignments
 const int PUL_PIN = 18;
 const int DIR_PIN = 19;
 const int ENA_PIN = 21;  // ENA− connected here, ENA+ tied to +5V
 
 // Motor & microstep specs
-const int stepsPerRev = 100;  // 1.8° × 1/8 microstep
+const int stepsPerRev = 1600;  // 1.8° × 1/8 microstep
 
 // Timing constants (µs)
 const unsigned int ENA_SETUP_US = 5;     // ENA must settle before DIR in single-pulse mode
 const unsigned int DIR_SETUP_US = 5;     // DIR must settle before PUL rising edge
 const unsigned int PULSE_HIGH_US = 500;   // keep PUL high for ≥ 2.5 µs
 const unsigned int PULSE_LOW_US = 500;    // low time between pulses
+
+// Current rotator task.
+int taskLength = 0;
+unsigned int taskTime = 0;
+
+// Length of loop.
+unsigned int LOOP_LEN = 3000; // Loop iterates every 3000 microseconds
+
+void pulse(int highDur, int lowDur) {
+    // 🌀 Generate pulses to rotate motor
+    digitalWrite(PUL_PIN, HIGH);
+    delayMicroseconds(highDur);  // Adjust for speed
+    digitalWrite(PUL_PIN, LOW);
+    delayMicroseconds(lowDur);
+}
+
+int subtaskLength() {
+
+}
 
 void setup() {
     Serial.begin(115200);
@@ -62,7 +74,7 @@ void loop() {
     delayMicroseconds(DIR_SETUP_US);
 
     for (int i = 0; i < stepsPerRev; i++) {
-        pulse();
+        pulse(PULSE_HIGH_US, PULSE_LOW_US);
     }
     delay(2000);
 
@@ -73,7 +85,7 @@ void loop() {
     delayMicroseconds(DIR_SETUP_US);
 
     for (int i = 0; i < stepsPerRev; i++) {
-        pulse();
+        pulse(PULSE_HIGH_US, PULSE_LOW_US);
     }
     delay(1000);
 }
