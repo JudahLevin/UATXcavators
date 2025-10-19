f233186d36b3f200e485f92d519c9cde6afd5ecd
Emessjay <jackprattporter@gmail.com>
2025-10-18 19:28:02 -0500

    motor controls working

--- software/TBM Controller/src/motor_test.cpp
+++ software/TBM Controller/src/motor_test.cpp
@@ -14,13 +14,6 @@
 // PURPLE: IO2 (25) -> PUL+
 // ======================================================================
 
-void pulse() {
-    // 🌀 Generate pulses to rotate motor
-    digitalWrite(PUL_PIN, HIGH);
-    delayMicroseconds(PULSE_HIGH_US);  // Adjust for speed
-    digitalWrite(PUL_PIN, LOW);
-    delayMicroseconds(PULSE_LOW_US);
-}
 // Pin assignments
 const int PUL_PIN = 18;
 const int DIR_PIN = 19;
@@ -32,8 +25,27 @@ const int stepsPerRev = 1600;  // 1.8° × 1/8 microstep
 // Timing constants (µs)
 const unsigned int ENA_SETUP_US = 5;     // ENA must settle before DIR in single-pulse mode
 const unsigned int DIR_SETUP_US = 5;     // DIR must settle before PUL rising edge
-const unsigned int PULSE_HIGH_US = 500;   // keep PUL high for ≥ 2.5 µs
-const unsigned int PULSE_LOW_US = 500;    // low time between pulses
+const unsigned int PULSE_HIGH_US = 250;   // keep PUL high for ≥ 2.5 µs
+const unsigned int PULSE_LOW_US = 250;    // low time between pulses
+
+// Length of loop.
+unsigned int LOOP_LEN = 3000; // Loop iterates every 3000 microseconds
+
+bool isOn = false;
+bool isReversed = false;
+
+void pulse() {
+    // 🌀 Generate pulses to rotate motor
+    digitalWrite(PUL_PIN, HIGH);
+    delayMicroseconds(PULSE_HIGH_US);  // Adjust for speed
+    digitalWrite(PUL_PIN, LOW);
+    delayMicroseconds(PULSE_LOW_US);
+}
+
+void safeWriteDir(int state) {
+    digitalWrite(DIR_PIN, state);
+    delayMicroseconds(DIR_SETUP_US);
+}
 
 void setup() {
     Serial.begin(115200);
@@ -52,28 +64,21 @@ void setup() {
 }
 
 void loop() {
-    // Ensure driver is enabled
-    digitalWrite(ENA_PIN, LOW);
-    delayMicroseconds(ENA_SETUP_US);
+    // Whenever a character is typed, runs loop body
+    if (Serial.available()) {
+        String keyword = Serial.readString();
+        switch (tolower(keyword.charAt(0))) {
+            case 's':
+                isOn = !isOn;
+                break;
+            case 'r':
+                isReversed = !isReversed;
+                safeWriteDir(isReversed);
+                break;
+        }
+    }
 
-    // Spin one revolution forward
-    Serial.println("Forward");
-    digitalWrite(DIR_PIN, HIGH);
-    delayMicroseconds(DIR_SETUP_US);
-
-    for (int i = 0; i < stepsPerRev; i++) {
+    if (isOn) {
         pulse();
     }
-    delay(2000);
-
-    // Spin one revolution backward
-    Serial.println("Reverse");
-    // ENA is already low (enabled)
-    digitalWrite(DIR_PIN, LOW);
-    delayMicroseconds(DIR_SETUP_US);
-
-    for (int i = 0; i < stepsPerRev; i++) {
-        pulse();
-    }
-    delay(1000);
 }
